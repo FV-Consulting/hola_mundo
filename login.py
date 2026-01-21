add1ef60d90556c81fcdbaabed607d85289eec70
fen-uah <econometriafenuah@gmail.com>
2026-01-21 15:14:18 -0300

    login

--- login.py
+++ login.py
@@ -1,10 +1,38 @@
 import streamlit as st
+import requests
 
-st.title("Login test")
+st.title("Ejemplo inicio de sesión")
+
+def login_screen():
+    st.header("Esta app es privada")
+    st.subheader("Por favor inicia sesión")
+    st.button(":material/login: Iniciar Sesión con Google", on_click=st.login)
 
 if not st.user.is_logged_in:
-    st.button("Login con Google", on_click=st.login)
+    login_screen()
     st.stop()
 
-st.success(f"Hola {st.user.name} — {st.user.email}")
-st.button("Logout", on_click=st.logout)
+with st.sidebar:
+    c1, c2 = st.columns([1, 3])
+
+    with c1:
+        if st.user.picture:
+            try:
+                r = requests.get(st.user.picture, timeout=10)
+                if r.status_code == 200:
+                    st.image(r.content, width=90)
+                else:
+                    st.warning("No se pudo cargar la imagen")
+            except Exception as e:
+                st.warning(f"Error al cargar imagen: {e}")
+        else:
+            st.info("El usuario no tiene imagen")
+
+    with c2:
+        st.markdown("### Información del usuario")
+        st.write(f"**Nombre:** {st.user.name}")
+        st.write(f"**Correo:** {st.user.email}")
+
+    st.button(":material/logout: Cerrar sesión", on_click=st.logout)
+
+st.success("✅ Sesión iniciada")
