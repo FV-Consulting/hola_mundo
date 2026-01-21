e5aab9882309eda434c414902eb62d6ce18fcde4
fen-uah <econometriafenuah@gmail.com>
2026-01-21 14:59:19 -0300

    intento 3 de inicio

--- login.py
+++ login.py
@@ -10,28 +10,29 @@ def login_screen():
 
 if not st.user.is_logged_in:
     login_screen()
-else:
-    with st.sidebar:
-        c1, c2 = st.columns([1, 3])
+    st.stop()
 
-        with c1:
-            if st.user.picture:
-                try:
-                    r = requests.get(st.user.picture, timeout=10)
-                    if r.status_code == 200:
-                        st.image(r.content, width=90)
-                    else:
-                        st.warning("No se pudo cargar la imagen")
-                except Exception as e:
-                    st.warning(f"Error al cargar imagen: {e}")
-            else:
-                st.info("El usuario no tiene imagen")
+with st.sidebar:
+    c1, c2 = st.columns([1, 3])
 
-        with c2:
-            st.markdown("### Información del usuario")
-            st.write(f"**Nombre:** {st.user.name}")
-            st.write(f"**Correo:** {st.user.email}")
+    with c1:
+        if st.user.picture:
+            try:
+                r = requests.get(st.user.picture, timeout=10)
+                if r.status_code == 200:
+                    st.image(r.content, width=90)
+                else:
+                    st.warning("No se pudo cargar la imagen")
+            except Exception as e:
+                st.warning(f"Error al cargar imagen: {e}")
+        else:
+            st.info("El usuario no tiene imagen")
 
-        st.button(":material/logout: Cerrar sesión", on_click=st.logout)
+    with c2:
+        st.markdown("### Información del usuario")
+        st.write(f"**Nombre:** {st.user.name}")
+        st.write(f"**Correo:** {st.user.email}")
 
-    st.success("✅ Sesión iniciada")
+    st.button(":material/logout: Cerrar sesión", on_click=st.logout)
+
+st.success("✅ Sesión iniciada")
